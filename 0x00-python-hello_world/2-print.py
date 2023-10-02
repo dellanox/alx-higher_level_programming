be6a635eaae283fc2273f4ffd92fbb5378ea38b1
Ozioma EMEREM <code.emerem@gmail.com>
2023-10-02 23:24:24 +0100

    2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 # Use the print function to print the desired message
-print("Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")
