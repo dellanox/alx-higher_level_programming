59ddf62fe1947202a581520dd0cfa6722ea7f0b4
Ozioma EMEREM <code.emerem@gmail.com>
2023-10-03 23:59:47 +0100

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,2 @@
-import dis
 def magic_calculation(a, b):
-    result = 98
-    result += a ** b
-    return result
-# Print the bytecode of the function for reference
-dis.dis(magic_calculation)
+    return (98 + a ** b)
