b7986457aed88dd2da787f89f463eadf8b95bc87
Ozioma EMEREM <code.emerem@gmail.com>
2023-10-05 22:49:56 +0100

    Redo task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-
-if len(sys.argv) == 1:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-if len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-if len(sys.argv) > 2:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-if len(sys.argv):
-    for i in range(1, len(sys.argv)):
-        print("{:d}:".format(i), sys.argv[i])
+    from sys import argv
+    if len(argv) == 1:
+        print("{0:d} arguments.".format(len(argv) - 1))
+    elif len(argv) > 1:
+        if len(argv) > 2:
+            print("{0:d} arguments:".format(len(argv) - 1))
+        else:
+            print("{0:d} argument:".format(len(argv) - 1))
+        for i in range(len(argv) - 1):
+            print("{0:d}: {1}".format(i + 1, argv[i + 1]))
