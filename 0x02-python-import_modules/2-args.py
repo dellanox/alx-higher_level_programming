0c5050c5e4ae1b5d938cd96f42517356c0d1a7f0
Ozioma EMEREM <code.emerem@gmail.com>
2023-10-05 22:58:25 +0100

    Make task 2 executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) == 1:
-        print("{0:d} arguments.".format(len(argv) - 1))
-    elif len(argv) > 1:
-        if len(argv) > 2:
-            print("{0:d} arguments:".format(len(argv) - 1))
-        else:
-            print("{0:d} argument:".format(len(argv) - 1))
-        for i in range(len(argv) - 1):
-            print("{0:d}: {1}".format(i + 1, argv[i + 1]))
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
